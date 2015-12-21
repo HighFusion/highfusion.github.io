2ed732f0892615e4c5fcf114d5d1fb516aaeb0cd
Vladimir Solovey <vladimir@highfusion.com>
2015-12-21 17:10:10 -0600

    Pick Your Perk email

---
+++ email/pick-your-perk-jason.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <?php
+        $siteUrl = getenv('siteUrl')
+    ?>
+</head>
+<body style="color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<table style="width: 100%">
+    <tr>
+        <td align="center">
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/new-york/pick-your-perk/hero.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="mailto:jason.sturtevant@archerhotel.com?subject=Pick Your Perk">
+                            <img src="<?= $siteUrl ?>/images/email/new-york/pick-your-perk/jason.sturtevant.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com/new-york" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/new-york-footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
