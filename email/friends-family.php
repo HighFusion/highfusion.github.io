a67993cdb8fb5731d67f2314ac7f5f79de461e43
Vladimir Solovey <vladimir@highfusion.com>
2016-03-13 12:41:17 -0500

    Add HMS logo for Austin

--- email/friends-family.php
+++ email/friends-family.php
@@ -6,7 +6,7 @@
     $siteUrl = getenv('siteUrl')
     ?>
 </head>
-<body style="color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<body style="background-color: #eee; color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
 <table style="width: 100%">
     <tr>
         <td align="center">
@@ -42,7 +42,21 @@
                 <tr>
                     <td>
                         <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/lodgeworks-rates/footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/shared/archer-footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/archer-footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/lodgeworks-rates/footer-family-notice.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
                 </tr>
