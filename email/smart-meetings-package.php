775d46e3b1573f8cf3236155e96e0506329b9f42
Vladimir Solovey <vladimir@highfusion.com>
2017-03-01 21:12:18 -0600

    Missing Google Plus

--- email/smart-meetings-package.php
+++ email/smart-meetings-package.php
@@ -40,11 +40,13 @@
                             <img src="<?= $siteUrl ?>/images/email/shared/footer-logo.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
-                    <td style="width: 400px; height: 78px;" colspan="2">
+                    <td style="width: 400px; height: 86px;" colspan="2">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <a href="http://facebook.com/archerhotelaustin"><img src="<?= $siteUrl ?>/images/email/shared/icon-facebook.jpg" /></a>&nbsp;
                         <a href="twitter.com/archerhotel"><img src="<?= $siteUrl ?>/images/email/shared/icon-twitter.jpg" /></a>&nbsp;
                         <a href="http://instagram.com/archerhotels"><img src="<?= $siteUrl ?>/images/email/shared/icon-instagram.jpg" /></a>&nbsp;
                         <a href="http://pinterest.com/archerhotel"><img src="<?= $siteUrl ?>/images/email/shared/icon-pinterest.jpg" /></a>&nbsp;
+                        <a href="https://plus.google.com/101685866202060759232"><img src="<?= $siteUrl ?>/images/email/shared/icon-googleplus.jpg" /></a>&nbsp;
                         <a href="https://www.youtube.com/channel/UCZ5MX16VUWxKo62fOBg1oEg"><img src="<?= $siteUrl ?>/images/email/shared/icon-youtube.jpg" /></a>&nbsp;
                     </td>
                 </tr>
