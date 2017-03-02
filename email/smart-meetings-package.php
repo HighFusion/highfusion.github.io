cf1bbdd5f9a04833563a0e1464e0264352d31e60
Vladimir Solovey <vladimir@highfusion.com>
2017-03-01 20:36:26 -0600

    Smart packages email updated

--- email/smart-meetings-package.php
+++ email/smart-meetings-package.php
@@ -11,28 +11,28 @@
     <tr>
         <td align="center">
             <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
-                <tr>
+                <tr colspan="3">
                     <td>
                         <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/brand/haute-sports.hero.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/brand/haute-sports.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
                 </tr>
-                <tr>
+                <tr colspan="3">
                     <td>
                         <a href="http://archerhotel.com/book/smart-meetings-package">
                             <img src="<?= $siteUrl ?>/images/email/shared/smart-meetings-package.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
                 </tr>
-                <tr>
+                <tr colspan="3">
                     <td>
                         <a href="mailto:?subject=Smart Meetings Package">
                             <img src="<?= $siteUrl ?>/images/email/shared/share-with.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
                 </tr>
-                <tr colspan="3" style="background: #000;">
+                <tr style="background: #000;">
                     <td style="width: 200px;">
                         <a href="http://archerhotel.com/new-york" target="_blank">
                             <img src="<?= $siteUrl ?>/images/email/shared/footer-new-york.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
@@ -46,6 +46,23 @@
                     <td style="width: 200px;">
                         <a href="http://archerhotel.com/napa" target="_blank">
                             <img src="<?= $siteUrl ?>/images/email/shared/footer-napa.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/florham-park" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-florham-park.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-redmond.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-burlington.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
                         </a>
                     </td>
                 </tr>
