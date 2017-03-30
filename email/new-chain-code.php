891c791a646bdd4467b19ce4ac5653f058b7f2c8
Vladimir Solovey <vladimir@highfusion.com>
2017-03-30 12:56:22 -0500

    re #673 Chain code email

---
+++ email/new-chain-code.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <?php
+        $siteUrl = getenv('siteUrl')
+    ?>
+</head>
+<body style="color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<table style="width: 100%">
+    <tr>
+        <td align="center">
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com/book/new-chain-code">
+                            <img src="<?= $siteUrl ?>/images/email/shared/new-chain-code.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="mailto:?subject=New Chain Code">
+                            <img src="<?= $siteUrl ?>/images/email/shared/share-with-friend.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr style="background: #000;">
+                    <td style="width: 200px;" rowspan="2">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-logo.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 400px; height: 86px;" colspan="2">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="http://facebook.com/archerhotelaustin"><img src="<?= $siteUrl ?>/images/email/shared/icon-facebook.jpg" /></a>&nbsp;
+                        <a href="http://twitter.com/archerhotel"><img src="<?= $siteUrl ?>/images/email/shared/icon-twitter.jpg" /></a>&nbsp;
+                        <a href="http://instagram.com/archerhotels"><img src="<?= $siteUrl ?>/images/email/shared/icon-instagram.jpg" /></a>&nbsp;
+                        <a href="http://pinterest.com/archerhotel"><img src="<?= $siteUrl ?>/images/email/shared/icon-pinterest.jpg" /></a>&nbsp;
+                        <a href="https://plus.google.com/101685866202060759232"><img src="<?= $siteUrl ?>/images/email/shared/icon-googleplus.jpg" /></a>&nbsp;
+                        <a href="https://www.youtube.com/channel/UCZ5MX16VUWxKo62fOBg1oEg"><img src="<?= $siteUrl ?>/images/email/shared/icon-youtube.jpg" /></a>&nbsp;
+                    </td>
+                </tr>
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <img src="<?= $siteUrl ?>/images/email/shared/footer-phone.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="mailto:archer@archerhotel.com">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-email.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/new-york" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-new-york.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/austin" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-austin.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/napa" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-napa.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/florham-park" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-florham-park.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-redmond.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-burlington.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
