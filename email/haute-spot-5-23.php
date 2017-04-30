0b6a86348f23bcebf40e2c0c3998e2cd3a11b11b
Vladimir Solovey <vladimir@highfusion.com>
2017-04-29 20:49:20 -0500

    re #739 New Haute Spot email

---
+++ email/haute-spot-5-23.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <?php
+        $siteUrl = getenv('siteUrl')
+    ?>
+</head>
+<body style="color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<table style="width: 100%">
+    <tr>
+        <td align="center">
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/austin/haute-spot-5-23.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="mailto:?subject=Smart Meetings Package">
+                            <img src="<?= $siteUrl ?>/images/email/shared/rsvp.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+			<?php include 'inc/footer.php';?>
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/new-york" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-new-york.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/austin" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-austin.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/napa" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-napa.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr style="background: #000;">
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com/florham-park" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-florham-park.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-redmond.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                    <td style="width: 200px;">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/shared/footer-burlington.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
