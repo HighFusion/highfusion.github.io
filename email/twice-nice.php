deecfc3ade3db14dc2af3cd9e671c411144c5a72
Vladimir Solovey <vladimir@highfusion.com>
2017-11-10 12:06:16 -0600

    re #1025 Brooklyn opening email

---
+++ email/twice-nice.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <?php
+        $siteUrl = getenv('siteUrl')
+    ?>
+</head>
+<body style="color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<table style="width: 100%">
+    <tr>
+        <td align="center">
+            <table style="width: auto; max-width: 600px; margin: 0 auto; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <a href="http://lodgeworks.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/brooklyn/twice-nice/header.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="http://hamptoninn3.hilton.com/en/hotels/new-york/hampton-inn-brooklyn-downtown-NYCBOHX/index.html">
+                            <img src="<?= $siteUrl ?>/images/email/brooklyn/twice-nice/main.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="http://lodgeworks.com">
+                            <img src="<?= $siteUrl ?>/images/email/brooklyn/twice-nice/footer.jpg" style="width: 600px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
