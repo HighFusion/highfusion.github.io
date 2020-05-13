51cc226143fcefe401e547e5bb42ef5e34481dff
Vladimir Solovey <vladimir@highfusion.com>
2020-05-13 14:47:38 -0500

    Changes to employee rate template

--- email/employee-rate.php
+++ email/employee-rate.php
@@ -5,81 +5,77 @@
     <?php
     $siteUrl = getenv('siteUrl')
     ?>
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
 </head>
 <body style="background-color: #eee; color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
-<table style="width: 100%;">
-    <tr>
-        <td align="left">
-            <table style="background-color: #faf5e5; width: 600px; max-width: 600px; margin: 0; vertical-align: middle;" cellspacing="5" cellpadding="0">
-                <tr>
-                    <td colspan="3" style="text-align: center">
-                        <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/header.jpg" />
-                        </a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=67720&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/austin.jpg" />
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=77757&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/burlington.jpg" />
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=76198&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/florham-park.jpg" />
-                        </a>
-                    </td>
-                </tr>  
-                <tr>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=70482&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/napa.jpg" />
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=60268&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/new-york.jpg" />
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=5998&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/redmond.jpg" />
-                        </a>
-                    </td>
-                </tr>   
-                <tr>
-                    <td>
-                        <a href="https://archerhotel.com/tysons">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/tysons.jpg" />
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://archerhotel.com">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/brand.jpg" />
-                        </a>
-                    </td>
-                    <td style="background-color: #d8ab27; text-align: center">
-                        <img src="<?= $siteUrl ?>/images/email/rates/employee/message.jpg" />
-                        <a href="https://www.facebook.com/search/top/?q=Archer%20Hotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/facebook.png" /></a>&nbsp;
-                        <a href="https://twitter.com/ArcherHotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/twitter.png" /></a>&nbsp;
-                        <a href="https://www.instagram.com/archerhotels/" target="_blank"><img src="<?= $siteUrl ?>/images/social/instagram.png" /></a>
-                    </td>
-                </tr>                                                                                      
-                <tr>
-                    <td colspan="3">
-                        <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/footer.jpg" />
-                        </a>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+    <table style="background-color: #faf5e5; width: 600px; max-width: 600px!important; margin: 0; vertical-align: middle;" cellspacing="5" cellpadding="0" width="600">
+        <tr>
+            <td colspan="3" width="600" style="text-align: center">
+                <a href="http://archerhotel.com" target="_blank">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/header.jpg" />
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td width="200">
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=67720&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/austin.jpg" />
+                </a>
+            </td>
+            <td width="200">
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=77757&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/burlington.jpg" />
+                </a>
+            </td>
+            <td>
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=76198&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/florham-park.jpg" />
+                </a>
+            </td>
+        </tr>  
+        <tr>
+            <td width="200" width="200">
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=70482&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/napa.jpg" />
+                </a>
+            </td>
+            <td width="200">
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=60268&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/new-york.jpg" />
+                </a>
+            </td>
+            <td width="200">
+                <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=5998&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/redmond.jpg" />
+                </a>
+            </td>
+        </tr>   
+        <tr>
+            <td width="200">
+                <a href="https://archerhotel.com/tysons">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/tysons.jpg" />
+                </a>
+            </td>
+            <td width="200">
+                <a href="https://archerhotel.com">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/brand.jpg" />
+                </a>
+            </td>
+            <td style="background-color: #d8ab27; text-align: center" width="200">
+                <img src="<?= $siteUrl ?>/images/email/rates/employee/message.jpg" />
+                <br>
+                <a href="https://www.facebook.com/search/top/?q=Archer%20Hotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/facebook.png" /></a>&nbsp;
+                <a href="https://twitter.com/ArcherHotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/twitter.png" /></a>&nbsp;
+                <a href="https://www.instagram.com/archerhotels/" target="_blank"><img src="<?= $siteUrl ?>/images/social/instagram.png" /></a>
+            </td>
+        </tr>                                                                                      
+        <tr>
+            <td colspan="3" width="600" >
+                <a href="http://archerhotel.com" target="_blank">
+                    <img src="<?= $siteUrl ?>/images/email/rates/employee/footer.jpg" />
+                </a>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
