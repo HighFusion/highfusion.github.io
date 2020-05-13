c624fadd2ba60bd7109c35fc1c0f026942e829c1
Vladimir Solovey <vladimir@highfusion.com>
2020-05-13 11:37:55 -0500

    re #2145 New email template for Employee rates

---
+++ email/employee-rate copy.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <?php
+    $siteUrl = getenv('siteUrl')
+    ?>
+</head>
+<body style="background-color: #eee; color: #222222; font-family: Helvetica, Arial, sans-serif; font-size: 14px;">
+<table style="width: 100%;">
+    <tr>
+        <td align="left">
+            <table style="width: 600px; max-width: 600px; margin: 0; vertical-align: top;" cellspacing="0" cellpadding="0">
+                <tr>
+                    <td>
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/header.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=67720&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/austin.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>   
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=77757&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/burlington.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>  
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=76198&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/florham-park.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>  
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=70482&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/napa.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>  
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=60268&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/new-york.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>  
+                <tr>
+                    <td>
+                        <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=5998&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/redmond.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>                                                                        
+                <tr>
+                    <td colspan="2">
+                        <a href="http://archerhotel.com" target="_blank">
+                            <img src="<?= $siteUrl ?>/images/email/rates/emp/footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
--- email/employee-rate.php
+++ email/employee-rate.php
@@ -10,60 +10,70 @@
 <table style="width: 100%;">
     <tr>
         <td align="left">
-            <table style="width: 600px; max-width: 600px; margin: 0; vertical-align: top;" cellspacing="0" cellpadding="0">
+            <table style="background-color: #faf5e5; width: 600px; max-width: 600px; margin: 0; vertical-align: middle;" cellspacing="5" cellpadding="0">
                 <tr>
-                    <td>
+                    <td colspan="3" style="text-align: center">
                         <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/header.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/header.jpg" />
                         </a>
                     </td>
                 </tr>
                 <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=67720&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/austin.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/austin.jpg" />
                         </a>
                     </td>
-                </tr>   
-                <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=77757&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/burlington.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/burlington.jpg" />
                         </a>
                     </td>
-                </tr>  
-                <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=76198&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/florham-park.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/florham-park.jpg" />
                         </a>
                     </td>
                 </tr>  
                 <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=70482&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/napa.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/napa.jpg" />
                         </a>
                     </td>
-                </tr>  
-                <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=60268&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/new-york.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/new-york.jpg" />
                         </a>
                     </td>
-                </tr>  
-                <tr>
                     <td>
                         <a href="https://book.archerhotel.com/?configcode=AHGC&hotel=5998&level=hotel&locale=en-US&promo=20EMPLOYEE&themecode=AHCT">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/redmond.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/redmond.jpg" />
                         </a>
                     </td>
-                </tr>                                                                        
+                </tr>   
                 <tr>
-                    <td colspan="2">
+                    <td>
+                        <a href="https://archerhotel.com/tysons">
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/tysons.jpg" />
+                        </a>
+                    </td>
+                    <td>
+                        <a href="https://archerhotel.com">
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/brand.jpg" />
+                        </a>
+                    </td>
+                    <td style="background-color: #d8ab27; text-align: center">
+                        <img src="<?= $siteUrl ?>/images/email/rates/employee/message.jpg" />
+                        <a href="https://www.facebook.com/search/top/?q=Archer%20Hotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/facebook.png" /></a>&nbsp;
+                        <a href="https://twitter.com/ArcherHotel" target="_blank"><img src="<?= $siteUrl ?>/images/social/twitter.png" /></a>&nbsp;
+                        <a href="https://www.instagram.com/archerhotels/" target="_blank"><img src="<?= $siteUrl ?>/images/social/instagram.png" /></a>
+                    </td>
+                </tr>                                                                                      
+                <tr>
+                    <td colspan="3">
                         <a href="http://archerhotel.com" target="_blank">
-                            <img src="<?= $siteUrl ?>/images/email/rates/employee/footer.jpg" style="display: block; width: auto; max-width: 100%; min-width: 100px;" />
+                            <img src="<?= $siteUrl ?>/images/email/rates/employee/footer.jpg" />
                         </a>
                     </td>
                 </tr>
